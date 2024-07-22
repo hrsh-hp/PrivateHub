3686aed907099b92801bbafa33ea878f67f37602
Harsh <parmarharsh901@gmail.com>
2024-07-23 00:56:07 +0530

    create and join room added with random room name

--- videochat/views.py
+++ videochat/views.py
@@ -1,18 +1,39 @@
+import random
+import string
 from django.shortcuts import render
-
+import uuid 
 # Create your views here.
+
+def generate_random_hash():
+    chars = string.ascii_lowercase
+    
+    part1 = ''.join(random.choice(chars) for _ in range(3))
+    part2 = ''.join(random.choice(chars) for _ in range(3))
+    part3 = ''.join(random.choice(chars) for _ in range(3))
+    
+    random_hash = f"{part1}-{part2}-{part3}"
+    
+    return random_hash
+
 def main_view(request):
     
-    global room 
-    room = 'Test'
+    # global room 
+    # room = 'Test'
     if request.method == "POST":
-        room = request.POST['room']
         
-    if room:
+        action = request.POST['action']
+        print(action)
+        
+        if action == 'create':
+            room = generate_random_hash()
+        elif action == 'join':
+            room = request.POST['room']
+        
         context = {'room' : room }
+        return render(request, 'videochat/main.html',context)
+    
     else:
-        context = {'room' : room }
-    return render(request, 'videochat/main.html',context)
+        return render(request, 'videochat/index.html')  
 
 
 def index(request):
