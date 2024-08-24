e047a8ee1ade0037cf86f0fc9cfc046b183ec39a
Harsh <parmarharsh901@gmail.com>
2024-08-25 01:55:04 +0530

    User model changes

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from authentication.models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ['email','first_name','is_email_verified', 'is_superuser','register_date']
+    list_filter = ['is_email_verified']
+    search_fields = ['email']
+    ordering = ['email']
+    
+admin.site.register(CustomUser, CustomUserAdmin)
--- privatehub/settings.py
+++ privatehub/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -68,7 +69,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
+CORS_ALLOWED_ORIGINS = [        
     NGROK_LINK,
 ]
 
